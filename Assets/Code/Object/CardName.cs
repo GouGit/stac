371f3361ef2fe4f8554e0c39889e246eaf6630cb
Jin-Hwan Kim <46664980+GouGit@users.noreply.github.com>
2019-08-06 21:12:05 +0900

    카드
    텍스트 매쉬추가

---
+++ Assets/Code/Object/CardName.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardName : MonoBehaviour
+{
+    public string sortingLayerName;
+    public int sortingOrder;
+    private new string name;
+    private TextMesh textMesh;
+
+    void Start () 
+    {
+        MeshRenderer mesh = GetComponent<MeshRenderer> ();
+        textMesh = GetComponent<TextMesh>();
+        mesh.sortingLayerName = sortingLayerName;
+        mesh.sortingOrder = sortingOrder;
+        ShowCard show = transform.parent.GetComponent<ShowCard>();
+        name = show.card.name;
+        textMesh.text = name;
+    }
+}
