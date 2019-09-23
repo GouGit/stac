79f5597f5431b0272c0148a4515abbb7ebca8dab
sweets125 <an060875@naver.com>
2019-09-23 19:01:07 +0900

    - 맵 선택창 캐릭터 HP 표시

---
+++ Assets/Code/UI/MapTreeUIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapTreeUIManager : MonoBehaviour
+{
+    // 플레이어의 HP를 표시할 이미지입니다.
+    public Image m_PlayerHPImage;
+
+    void Start()
+    {
+        if (m_PlayerHPImage == null)
+            m_PlayerHPImage = GameObject.Find("HP_Image").GetComponent<Image>();
+    }
+
+    void LateUpdate()
+    {
+        if (m_PlayerHPImage != null)
+            m_PlayerHPImage.fillAmount = (float)Knight.instance.HP / (float)Knight.instance.MaxHP;
+    }
+}
