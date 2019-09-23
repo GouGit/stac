fd4f867f4feeb47affdef36bdfd9a6b79599a655
Jin-Hwan Kim <46664980+GouGit@users.noreply.github.com>
2019-09-23 16:11:51 +0900

    강화표시

---
+++ Assets/Code/Object/CardText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardText : MonoBehaviour
+{
+    public string sortingLayerName;
+    public int sortingOrder;
+    private string cost;
+    private TextMesh textMesh;
+
+    void Start () 
+    {
+        MeshRenderer mesh = GetComponent<MeshRenderer> ();
+        textMesh = GetComponent<TextMesh>();
+        mesh.sortingLayerName = sortingLayerName;
+        mesh.sortingOrder = sortingOrder;
+        ShowCard show = transform.parent.GetComponent<ShowCard>();
+        if(show.level > 0)
+            textMesh.text = "+" + show.level;
+    }
+}
