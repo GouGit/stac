c40f7e8b57290b0e8840cb8876f62ae2ce404b24
sweets125 <an060875@naver.com>
2019-09-23 16:39:49 +0900

    - 진동 설정 - 힐 파티클 - 머시기

---
+++ Assets/Code/UI/BattleUIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleUIManager : MonoBehaviour
+{
+    public void LoadTitle()
+    {
+        SceneLoader.Instance.LoadSceneWithFade("Title");
+    }
+
+    public void LoadMapTree()
+    {
+        SceneLoader.Instance.LoadSceneWithFade("MapTree");
+    }
+}
